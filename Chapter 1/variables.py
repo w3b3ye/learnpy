fdb986baf217eb1b292d06c93bbf5d9e615f5d7b
w3b3ye <w3b3ye@gmail.com>
2019-10-04 21:15:53 +0400

    Using variables

---
+++ Chapter 1/variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Different kind of variables in python
+
+n = 3 # integer number
+
+address = "Flat 123, Building ABC, Area XYZ, Dubai" # String
+
+employee = {'age':45, 'role':'CISO','SSN':'ABC123',} # Dictionary
+
+print(n)
+
+print(address)
+
+print(employee)
+
+exit(0)
+
+
+
